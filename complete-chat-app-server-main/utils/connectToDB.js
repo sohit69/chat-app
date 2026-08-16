ff914a018a2de737adfbcf0220402e2894750797
sohit <sapehiyasohit@gmail.com>
2026-08-16 14:02:36 +0530

    fix import

--- complete-chat-app-server-main/utils/connectToDB.js
+++ complete-chat-app-server-main/utils/connectToDB.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+ 
+    await
+     mongoose.connect(process.env.MONGO_URI);
     console.log("DB Connected");
-  } catch (error) {
-    console.log("DB Connection Error:", error);
+ 
+    console.error("DB Connection Error:", error);
   }
-};
 
 export default connectToDB;
